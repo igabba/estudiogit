78546165aca8cb054f5b5defc852c4f8ff7b41a9
igabbarini <ignaciogabbarini@gmail.com>
2010-05-04 10:20:44 -0300

    import inicial de proyecto de prueba maven

---
+++ ejemplo/src/main/java/com/mycompany/ejemplo/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.ejemplo;
+
+/**
+ *
+ * @author igabbarini
+ */
+public class NewClass implements NewInterface{
+
+    public void prueba() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ ejemplo/src/main/java/com/mycompany/ejemplo/NewInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.ejemplo;
+
+/**
+ *
+ * @author igabbarini
+ */
+public interface NewInterface {
+
+    public void prueba();
+
+}
